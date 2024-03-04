5c41bf970328ffeccad8e8c3d77475bafceb3085
JuanDavidSerna <jserna@cloudlabs.co>
2024-03-04 11:26:32 -0500

    PracticJava8Lambda

--- src/org/juandavid/java8/lambda/EjemploFunction.java
+++ src/org/juandavid/java8/lambda/EjemploFunction.java
@@ -1,6 +1,9 @@
 package org.juandavid.java8.lambda;
 
+import java.util.function.BiConsumer;
+import java.util.function.BiFunction;
 import java.util.function.Function;
+import java.util.function.IntFunction;
 
 public class EjemploFunction {
 
@@ -10,5 +13,21 @@ public class EjemploFunction {
         String resultado = function.apply("Juan David");
 
         System.out.println(resultado);
+
+        Function<String, String> function2 = String::toUpperCase;
+        System.out.println(function2.apply("Pedro pascal"));
+
+
+        BiFunction<String, String, String> biFunction = (a,b)-> a.toUpperCase().concat(b.toUpperCase());
+
+        String r2 = biFunction.apply("paola", " andrea");
+
+        System.out.println(r2);
+
+        BiFunction<String, String, Integer> biFunction2 = String::compareTo;
+        System.out.println(biFunction2.apply("laura", "laura"));
+
+        BiFunction<String, String, String> biFunction3 = String::concat;
+        System.out.println(biFunction3.apply("santiago", " jose"));
     }
 }
